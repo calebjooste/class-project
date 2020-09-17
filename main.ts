a799c6b6ac32a294d8624f9013531b33607491a3
calebjooste <69714925+calebjooste@users.noreply.github.com>
2020-09-17 12:27:33 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,4 +6,15 @@ while (Counter < endstate) {
     led.toggle(0, 0)
     led.toggle(0, 0)
     Counter += 1
+    basic.pause(1000)
+}
+Counter += 1
+// unpin
+// 
+while (Counter < endstate) {
+    led.toggle(0, Counter)
+    led.toggle(0, 0)
+    led.plot(0, 2)
+    Counter = 0
+    basic.pause(200)
 }
